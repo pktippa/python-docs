9088eb59662757582363ee9dc52244b8ea708239
Pradeep Kumar Tippa <pktippa@gmail.com>
2017-04-04 00:48:18 +0530

    Added Datatypes

---
+++ datatypes.py
@@ -0,0 +1,11 @@
+int_type = 10
+# long_type = 51924361L # For Python 2
+long_type = 535633629843
+float_type = 89.78
+complex_type = 4+3j
+string_type='hello data types.'
+print('Integer ', int_type, ' has datatype', type(int_type))
+print('Long ', long_type, ' has datatype', type(long_type)) # Python 2 prints as long where as python 3 prints as int
+print('Complex ', complex_type, ' has datatype', type(complex_type))
+print('Float ', float_type, ' has datatype', type(float_type))
+print('Integer ', string_type, ' has datatype', type(string_type))
