88583dafb858513a4cc6196aa1702c11474fafac
Pradeep Kumar Tippa <pktippa@gmail.com>
2017-04-05 23:01:54 +0530

    Added String Datatype

---
+++ datatypes/string.py
@@ -0,0 +1,31 @@
+pancard_number="AABGT6715H"
+
+#Length of the string
+print("Length of the PAN card number:", len(pancard_number))
+
+#Concatenating two strings
+name1 ="PAN "
+name2="card"
+name=name1+name2
+print(name)
+
+print("Iterating the string using range()")
+for index in range(0,len(pancard_number)):
+    print(pancard_number[index])
+    
+print("Iterating the string using keyword in")
+for value in pancard_number:
+    print(value)
+
+print("Searching for a character in string")
+if "Z" in pancard_number:
+    print("Character present")
+else:
+    print("Character is not present")
+
+#Slicing a string
+print("The numbers in the PAN card number:", pancard_number[5:9])
+print("Last but one 3 characters in the PAN card:",pancard_number[-4:-1])
+
+pancard_number[2]="A" #This line will result in an error, i.e., string is immutable
+print(pancard_number)
