2a330773f0f93a443af58677df636a610cf82850
Pradeep Kumar Tippa <pktippa@gmail.com>
2017-04-05 23:10:30 +0530

    Added Set to collections

---
+++ collections/set.py
@@ -0,0 +1,10 @@
+# Set
+#Removes the duplicates from the given group of values to create the set.
+flight_set = {500,520,600,345,520,634,600,500,200,200}
+print("Flight Set : ", flight_set)
+
+# Converting List into Set
+passengers_list=["George", "Annie", "Jack", "Annie", "Henry", "Helen", "Maria", "George", "Jack", "Remo"]
+unique_passengers=set(passengers_list)#	set function - removes the duplicates from the list and returns a set
+
+print("Unique Passengers : ", unique_passengers)
