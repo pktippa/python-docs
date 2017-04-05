9cee8fde932b58fb7f18674cc2b1af75de1bada2
Pradeep Kumar Tippa <pktippa@gmail.com>
2017-04-05 23:43:56 +0530

    Added Dictionary

---
+++ collections/dictionary.py
@@ -0,0 +1,14 @@
+# Creating a dictionary	
+crew_details = { 
+    "Pilot":"Kumar",
+    "Co-pilot":"Raghav",
+    "Head-Strewardess":"Malini",
+    "Stewardess":"Mala" 
+}
+
+# Accessing the value using key	
+print(crew_details["Pilot"])
+
+# Iterating through the dictionary	
+for key,value in crew_details.items():
+     print(key,":",value)	
