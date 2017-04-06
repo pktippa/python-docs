5e05503a21de64bba91f5582894b926b3dac57fd
Pradeep Kumar Tippa <pktippa@gmail.com>
2017-04-06 20:45:30 +0530

    Updated with Parameterized pytest cases

--- test/pytest_code.py
+++ test/pytest_code.py
@@ -2,9 +2,20 @@
 import pytest
 from topackage.actualcode import boarding # Inorder to import a file we can just convert the folder to package by adding empty __init__.py
 def test_boarding_1():
-        result=boarding(3)
-        assert result==1
+    result=boarding(3)
+    assert result==1
                 
 def test_boarding_2():
-        result=boarding(28)
-        assert result==2
+    result=boarding(28)
+    assert result==2
+
+# Parameterized pytest cases.
+# Instead of writing multiple test cases for same functionality we can write
+# one pytest test case with a list of tuples.
+
+values = [(0,-1),(1,1),(25,1),(100,2),(199,3),(201,-1)]
+
+@pytest.mark.parametrize("seat_number,expected_value",values)
+def test_borading(seat_number, expected_value):
+    result = boarding(seat_number)
+    assert result == expected_value
--- test/topackage/actualcode.py
+++ test/topackage/actualcode.py
@@ -1,10 +1,9 @@
 #Program to be tested
 
 def boarding(seat_number):
-    print("hello world")
     if seat_number >= 1 and seat_number <= 25:
         batch_no = 1
-    elif seat_number >= 26 or  seat_number <= 100:
+    elif seat_number >= 26 and  seat_number <= 100:
         batch_no = 2
     elif seat_number >= 101 and seat_number <= 200:
         batch_no = 3
