f117da8c8d7126756c9c3b2c7a812055b4efe8de
Pradeep Kumar Tippa <pktippa@gmail.com>
2017-04-08 18:02:52 +0530

    Updated Dictionary

--- collections/dictionary.py
+++ collections/dictionary.py
@@ -11,4 +11,14 @@ print(crew_details["Pilot"])
 
 # Iterating through the dictionary	
 for key,value in crew_details.items():
-     print(key,":",value)	
+     print(key,":",value)
+
+# Built in Functions
+crew_details={
+              "Pilot":"Kumar",
+              "Co-pilot":"Raghav",
+              "Head-Strewardess":"Malini",
+              "Stewardess":"Mala"
+              }
+crew_details.get("Pilot") # Returns the value for given key. If the given key is not found, returns None
+crew_details.update({"Flight Attendant":"Jane", "Co-pilot":"Henry"}) # Updates the dictionary with the given key-value pairs.     
